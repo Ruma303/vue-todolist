92734a3b6ef496cb947e66af6390a64abad63e6d
Ruma <matteoh8@hotmail.it>
2022-11-15 13:49:55 +0100

    aggiustamenti interfaccia

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 const app = new Vue ({
    el: '#app',
     data: {
-        text: '',
-        done: false,
-    },
+        todos: {
+            text: '',
+            done: false,
+        }, newTodo: '',
+    }, methods: {
+        addTodo() {
+            if(this.newTodo.trim())
+            {this.todo.push(this.newTodo.trim());
+            this.newTodo = '';}
+        }, deleteTodo(index) {
+            this.todo.splice(index,1);
+        }, todo() {
+            //crea questo metodo
+        }
+    }
 });
