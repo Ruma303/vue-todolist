f4ce12dd4391ae700957e03352ecca4d63aae975
Ruma <matteoh8@hotmail.it>
2022-11-15 15:40:37 +0100

    input azzerato

--- js/script.js
+++ js/script.js
@@ -11,16 +11,40 @@ const app = new Vue ({
             task: 'cucinare', 
             status: false,
         },], 
-        newTodo: '',
+        newTask: '', //proprietà per pushare nuove task nell'array todos
+        newStatus: false,
     }, methods: {
-        addTodo() {
-            if(this.newTodo.trim())
-            {this.todo.push(this.newTodo.trim());
-            this.newTodo = '';}
-        }, deleteTodo(index) {
-            this.todo.splice(index,1);
-        }, todo() {
-            //crea questo metodo
-        }
+        addTodo() {//funzione per aggiungere nuove task
+
+            //solo se c'è scritto qualcosa aggiungi 
+            if (this.newTask.length === 0) return;
+            this.todos.push({ 
+                task: this.newTask, //il task lo pusha, top
+               
+                // ----------->PROBLEMA QUI: NON PUSHA LO STATUS: FALSE <--------------
+                status: this.newStatus,
+                
+            });
+            //azzerare il campo input
+            this.newTask = '',
+            //oppure cambiare stato fuori dal push
+            console.log(this.newTask.status) //mi da undefined 
+            console.log(this.todos) //controllo l'intera array di oggetti e pare che ci sia
+
+             //alternativa vista da Henri non funziona
+
+            /*if (this.newTask.trim())
+            {this.todos.push(this.newTask.trim());
+            this.newTask = '';
+            console.log(this.newTask.trim()) 
+            } */
+
+
+
+        }, deleteTodo(i) { //metodo per cancellare i task
+            this.todos.splice(i,1);
+        }, //se cliccando la task cambia da true e a false, e sbarrala
+        taskDone () {this.status === true ? 'false' : ''}
+        
     }
 });
