914db4a38226b230d16b586fbb26b75892889651
Ruma <matteoh8@hotmail.it>
2022-11-16 09:55:10 +0100

    classe .done da fare singolarmente

--- js/script.js
+++ js/script.js
@@ -13,38 +13,26 @@ const app = new Vue ({
         },], 
         newTask: '', //proprietà per pushare nuove task nell'array todos
         newStatus: false,
-    }, methods: {
+    },  methods: {
         addTodo() {//funzione per aggiungere nuove task
-
             //solo se c'è scritto qualcosa aggiungi 
             if (this.newTask.length === 0) return;
             this.todos.push({ 
                 task: this.newTask, //il task lo pusha, top
-               
-                // ----------->PROBLEMA QUI: NON PUSHA LO STATUS: FALSE <--------------
-                status: this.newStatus,
-                
+                status: this.newStatus, //push status
             });
             //azzerare il campo input
             this.newTask = '',
+
             //oppure cambiare stato fuori dal push
             console.log(this.newTask.status) //mi da undefined 
             console.log(this.todos) //controllo l'intera array di oggetti e pare che ci sia
 
-             //alternativa vista da Henri non funziona
-
-            /*if (this.newTask.trim())
-            {this.todos.push(this.newTask.trim());
-            this.newTask = '';
-            console.log(this.newTask.trim()) 
-            } */
-
-
-
         }, deleteTodo(i) { //metodo per cancellare i task
             this.todos.splice(i,1);
-        }, //se cliccando la task cambia da true e a false, e sbarrala
-        taskDone () {this.status === true ? 'false' : ''}
-        
-    }
+        },
+        isDone (i) {
+            this.newStatus = !this.newStatus;
+        },
+    },
 });
